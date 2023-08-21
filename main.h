19f1962e570d076f745f0e0ab8e4e8d81e84770b
Hlogie01 <mllehlogonolo@gmail.com>
2023-08-21 14:16:38 -0700

    printf

---
+++ main.h
@@ -0,0 +1,39 @@
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#ifndef MAIN_H
+
+#define BUFF_SIZE 1024
+#define UNUSED(x) (void)(x)
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+
